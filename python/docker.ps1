042b0d4a7f9eaac4663966037a89423e90a57ce2
Karol <karol.przybylak1@gmail.com>
2022-09-17 15:51:08 +0200

    first commit

---
+++ python/docker.ps1
@@ -0,0 +1,5 @@
+﻿#docker ps 
+cd .\mqttToDb 
+docker buildx build \
+--push \
+--platform linux/arm64/v8,linux/amd64 \ --tag your-username/multiarch-example:latest .
---
+++ python/mqttToDb/main.py
@@ -0,0 +1,44 @@
+import paho.mqtt.client as mqtt
+from questdb.ingress import Sender
+from datetime import datetime
+import json
+
+MQTT_TOPIC = [("/v1/data/+/+/", 0)]
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_TOPIC)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    string = msg.topic.split("/")
+    json_payload = json.loads(msg.payload)
+    dataType = string[4]
+    with Sender('10.128.0.50', 9009) as sender:
+        sender.row(
+            'sensors_'+dataType,
+            symbols={'device': json_payload['device'] , 'sensor': json_payload['sensor'] },
+            columns={dataType: json_payload[dataType]},
+            at=datetime.fromtimestamp(json_payload['time']) )
+        sender.flush()
+
+#clientdb = InfluxDBClient("10.128.0.50", 9009, '', '', 'test')
+#clientdb.create_database('test')
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("10.128.0.50", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
+
